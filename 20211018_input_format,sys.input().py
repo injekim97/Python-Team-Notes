9526e3103a57adbc93763074e7013f891efba50a
injekim97 <76692644+injekim97@users.noreply.github.com>
2021-10-19 03:10:02 +0900

    20211018_input_format,sys.input().py

--- 20211018_input_format,sys.input().py
+++ 20211018_input_format,sys.input().py
@@ -1,29 +1,22 @@
-# -*- coding: utf-8 -*-
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.0
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
+#!/usr/bin/env python
+# coding: utf-8
 
-# +
+# In[1]:
+
+
 # 코딩 테스트 기본 입출력 양식
 
 # 입력 예시 : 1 2 3 4 5
 a = list(map(int,input().split()))
 print(a)
 
-# +
+
+# In[1]:
+
+
 # 코딩 테스트 빠르게 입력 받기(시간 줄이는 것)
 import sys
 
 data = input(sys.stdin.readline().rstrip())
 print(data)
+
