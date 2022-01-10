a07fca16d3df3a542307bfbc61763fda5412e7d4
In Je Kim <injekim97@naver.com>
2022-01-10 11:47:12 +0900

    professional-code

--- Inflearn - Algorithm_Problem_Solving/소수2.py
+++ Inflearn - Algorithm_Problem_Solving/소수2.py
@@ -10,7 +10,7 @@ for i in range(2,N+1):
     if check_list[i] == 0 :
         cnt += 1
 
-	# range(start,end,step(건너뜀)) , step 배수라고 생각
+	# range(start,end,step(건너뜀)) , step 배수라고 생각.
         # 2~20 까지의 2의 배수들을 전부 1로 값 지정함 -> 1은 소수가 아니라는 것을 뜻함
         for j in range(i,N+1,i) :
             check_list[j] = 1        
